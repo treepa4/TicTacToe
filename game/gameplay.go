730380754a3c3b7bc13e7573856acb457b28e363
treepa4 <treepa4@asuma.ru>
2026-06-18 16:37:09 +0300

    крч доделал базу

---
+++ game/gameplay.go
@@ -0,0 +1,107 @@
+package game
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Player struct {
+	Name   string
+	Symbol string
+}
+
+type Game struct {
+	Board         [3][3]string
+	HumanPlayer   Player
+	AiPlayer      Player
+	CurrentPlayer Player
+}
+
+func (g *Game) AiCall() {
+	fmt.Println("Ход компьютера...")
+	for {
+		row := rand.Intn(3)
+		col := rand.Intn(3)
+		if g.Board[row][col] == "_" {
+			g.Board[row][col] = g.AiPlayer.Symbol
+			break
+		}
+	}
+	return
+}
+
+func (g *Game) HumanCall(row, col int) bool {
+	if row > 2 || col > 2 || row < 0 || col < 0 {
+		fmt.Println("Неверные координаты")
+		return false
+	}
+	if g.Board[row][col] == "_" {
+		g.Board[row][col] = g.HumanPlayer.Symbol
+		return true
+	}
+	fmt.Println("Клетка занята")
+	return false
+}
+
+func NewGame(HumanSymbol string) *Game {
+	aiSymbol := "o"
+	if HumanSymbol == "o" {
+		aiSymbol = "x"
+	}
+	g := &Game{
+		HumanPlayer: Player{
+			Name:   "Игрок",
+			Symbol: HumanSymbol,
+		},
+		AiPlayer: Player{
+			Name:   "Компьютер",
+			Symbol: aiSymbol,
+		},
+	}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			g.Board[i][j] = "_"
+		}
+	}
+	if g.HumanPlayer.Symbol == "x" {
+		g.CurrentPlayer = g.HumanPlayer
+	} else {
+		g.CurrentPlayer = g.AiPlayer
+	}
+	return g
+}
+
+func (g *Game) PrintBoard() {
+	for row := 0; row < 3; row++ {
+		if row < 2 {
+			fmt.Printf(" %s | %s | %s\n---|---|---\n", g.Board[row][0], g.Board[row][1], g.Board[row][2])
+		} else {
+			fmt.Printf(" %s | %s | %s\n", g.Board[row][0], g.Board[row][1], g.Board[row][2])
+		}
+	}
+}
+
+func (g *Game) CheckWin(s string) bool {
+	for i := 0; i < 3; i++ {
+		if (g.Board[i][0] == s && g.Board[i][1] == s && g.Board[i][2] == s) ||
+			(g.Board[0][i] == s && g.Board[1][i] == s && g.Board[2][i] == s) {
+			return true
+		}
+	}
+	if (g.Board[0][0] == s && g.Board[1][1] == s && g.Board[2][2] == s) ||
+		(g.Board[0][2] == s && g.Board[1][1] == s && g.Board[2][0] == s) {
+		return true
+	}
+	return false
+}
+
+func (g *Game) CheckDraw() bool {
+	for row := 0; row < 3; row++ {
+		for col := 0; col < 3; col++ {
+			if g.Board[row][col] == "_" {
+				return false
+			}
+		}
+	}
+	return true
+}
--- main.go
+++ main.go
@@ -1,5 +1,72 @@
 package main
 
-func main() {
+import (
+	"TicTacToe/game"
+	"TicTacToe/greeting"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	greeting.Greeting()
+
+	fmt.Printf("Введите за кого вы будете играть(x/o): ")
+	scan := bufio.NewScanner(os.Stdin)
+	if ok := scan.Scan(); !ok {
+		fmt.Println("Scan failed")
+		return
+	}
+	humanS := scan.Text()
+	switch humanS {
+	case "х":
+		humanS = "x"
+	case "0":
+		humanS = "o"
+	case "о":
+		humanS = "o"
+	case "1":
+		humanS = "x"
+	case "2":
+		humanS = "o"
+	default:
+		humanS = strings.TrimSpace(strings.ToLower(humanS))
+	}
+	g := game.NewGame(humanS)
+	fmt.Println("Игровое поле: ")
+	g.PrintBoard()
+	for {
+		if g.CurrentPlayer.Name == "Игрок" {
+			var row, col int
+			for {
+				fmt.Println("Твой ход!")
+				_, err := fmt.Scanln(&row, &col)
+				if err != nil {
+					fmt.Println("Scan failed")
+					continue
+				}
+				if g.HumanCall(row-1, col-1) {
+					break
+				}
+
+			}
+		} else {
+			g.AiCall()
+		}
+		g.PrintBoard()
+		if g.CheckWin(g.CurrentPlayer.Symbol) {
+			fmt.Println("Партия завершилась победой ", g.CurrentPlayer.Name)
+			break
+		}
+		if g.CheckDraw() {
+			fmt.Println("Партия завершилась ничьей!")
+			break
+		}
+		if g.CurrentPlayer.Name == g.HumanPlayer.Name {
+			g.CurrentPlayer = g.AiPlayer
+		} else {
+			g.CurrentPlayer = g.HumanPlayer
+		}
+	}
 }
